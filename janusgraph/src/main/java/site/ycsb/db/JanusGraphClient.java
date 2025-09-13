ea25c073e78e219a82e25d7642167edd2d32348b
Ayush Pandey <ayushpandey8439@gmail.com>
2025-09-13 22:31:14 +0200

    Add janusgraph driver

---
+++ janusgraph/src/main/java/site/ycsb/db/JanusGraphClient.java
@@ -0,0 +1,222 @@
+package site.ycsb.db;
+
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import site.ycsb.ByteIterator;
+import site.ycsb.DB;
+
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import site.ycsb.DBException;
+import site.ycsb.Status;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Properties;
+
+public class JanusGraphClient extends DB{
+  Client client;
+  private static Properties props = new Properties();
+
+  @Override
+  public void init() throws DBException {
+    System.out.println("Starting Janusgraph client");
+    props = getProperties();
+    System.out.println(props);
+
+    if(props.getProperty("DBTYPE")==null){
+      System.out.println("DBTYPE must be provided");
+      throw new DBException("DBTYPE must be provided");
+    }
+    System.out.println("DBTYPE: "+props.getProperty("DBTYPE"));
+
+    if(props.getProperty("DBURI")==null){
+      System.out.println("DBURI must be provided");
+      throw new DBException("DBURI must be provided");
+    }
+    System.out.println("DBURI: "+props.getProperty("DBURI"));
+    try{
+      URI uri = new URI(props.getProperty("DBURI"));
+
+      Cluster cluster = Cluster.build()
+          .addContactPoint(uri.getHost())
+          .port(uri.getPort())
+          .create();
+
+      this.client = cluster.connect();
+
+      System.out.println("Connected to Janusgraph successfully");
+    } catch (Exception e) {
+      System.out.println("Failed to connect to Janusgraph database: " + e.getMessage());
+      throw new DBException(e);
+    }
+  }
+
+
+  @Override
+  public Status addVertex(String label, String id, Map<String, ByteIterator> properties) {
+    int status = 0;
+    try {
+      this.client.submit("g.addV(l).property('i', i)", Map.of("l", label, "i", id)).all().join();
+      for (Map.Entry<String, ByteIterator> entry : properties.entrySet()) {
+        this.client.submit("g.V().has(\"i\", i).property(k, v)", Map.of("i", id, "k", entry.getKey(), "v", entry.getValue().toString())).all().join();
+      }
+
+      return Status.OK;
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+  @Override
+  public Status addEdge(String label, String id, String from, String to, Map<String, ByteIterator> properties) {
+    int status = 0;
+    try {
+      this.client.submit("g.V().has('i', from).as('a').V().has('i', to).addE(l).from('a').property('i', i)", Map.of("from", from, "to", to, "l", label, "i", id)).all().join();
+      for (Map.Entry<String, ByteIterator> entry : properties.entrySet()) {
+        this.client.submit("g.E().has('i', i).property(k, v)", Map.of("i", id, "k", entry.getKey(), "v", entry.getValue().toString())).all().join();
+      }
+      return Status.OK;
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+
+  }
+
+  @Override
+  public Status setVertexProperty(String id, String key, ByteIterator value) {
+    try{
+      this.client.submit("g.V().has('i', i).property(k, v)", Map.of("i", id, "k", key, "v", value.toString())).all().join();
+      return Status.OK;
+    } catch (Exception e){
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status setEdgeProperty(String id, String key, ByteIterator value) {
+    try {
+      this.client.submit("g.E().has('i', i).property(k, v)", Map.of("i", id, "k", key, "v", value.toString())).all().join();
+      return Status.OK;
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status removeVertex(String id) {
+    int status = 0;
+    try {
+      this.client.submit("g.V().has('i', i).drop()", Map.of("i", id)).all().join();
+      return Status.OK;
+
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status removeEdge(String id) {
+    int status = 0;
+    try {
+      this.client.submit("g.E().has('i', i).drop()", Map.of("i", id)).all().join();
+      return Status.OK;
+    } catch (Exception e) {
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status removeVertexProperty(String id, String key) {
+    try{
+      this.client.submit("g.V().has('i', i).properties(k).drop()", Map.of("i", id, "k", key)).all().join();
+      return Status.OK;
+    }catch (Exception e){
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status removeEdgeProperty(String id, String key) {
+    try{
+      this.client.submit("g.E().has('i', i).properties(k).drop()", Map.of("i", id, "k", key)).all().join();
+      return Status.OK;
+    }catch (Exception e){
+      return Status.ERROR;
+    }
+  }
+
+
+
+  @Override
+  public Status getVertexCount() {
+    try{
+      this.client.submit("g.V().count()");
+      return Status.OK;
+    }
+    catch (Exception e){
+      System.out.println("Exception in getVertexCount: " + e.getMessage());
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status getEdgeCount() {
+    try{
+      this.client.submit("g.E().count()");
+      return Status.OK;
+    }
+    catch (Exception e){
+      System.out.println("Exception in getEdgeCount: " + e.getMessage());
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status getEdgeLabels() {
+    try{
+      this.client.submit("g.E().label().dedup()");
+      return Status.OK;
+    }
+    catch (Exception e){
+      System.out.println("Exception in getEdgeLabels: " + e.getMessage());
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status getVertexWithProperty(String key, ByteIterator value) {
+    try{
+      this.client.submit("g.V().has(k, v)", Map.of("k", key, "v", value.toString()));
+      return Status.OK;
+    }
+    catch (Exception e){
+      System.out.println("Exception in getVertexWithProperty: " + e.getMessage());
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status getEdgeWithProperty(String key, ByteIterator value) {
+    try{
+      this.client.submit("g.E().has(k, v)", Map.of("k", key, "v", value.toString()));
+      return Status.OK;
+    }
+    catch (Exception e){
+      System.out.println("Exception in getEdgeWithProperty: " + e.getMessage());
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status getEdgesWithLabel(String label) {
+    try{
+      this.client.submit("g.E().hasLabel(l)", Map.of("l", label));
+      return Status.OK;
+    }
+    catch (Exception e){
+      System.out.println("Exception in getEdgesWithLabel: " + e.getMessage());
+      return Status.ERROR;
+    }
+  }
+}
